215f95885f394494b0ff3a6b6cb556b484b49b33
srisurya-m <122385602+srisurya-m@users.noreply.github.com>
2025-11-21 11:51:01 +0530

    modified butterfly.v
    Removed copyright notice and comments from butterfly.v

--- src/butterfly.v
+++ src/butterfly.v
@@ -1,27 +1,4 @@
-/*
- * From our research paper "High-Performance Hardware Implementation of CRYSTALS-Dilithium"
- * by Luke Beckwith, Duc Tri Nguyen, Kris Gaj
- * at George Mason University, USA
- * https://eprint.iacr.org/2021/1451.pdf
- * =============================================================================
- * Copyright (c) 2021 by Cryptographic Engineering Research Group (CERG)
- * ECE Department, George Mason University
- * Fairfax, VA, U.S.A.
- * Author: Luke Beckwith
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *     http://www.apache.org/licenses/LICENSE-2.0
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- * =============================================================================
- * @author   Luke Beckwith <lbeckwit@gmu.edu>
- */
-
-
+`default_nettype wire
 `timescale 1ns / 1ps
 
 module butterfly(
@@ -219,7 +196,7 @@ module butterfly(
             if (ajlen3[0] == 1)
                 ajlen5 <= (ajlen3 >> 1) + (DILITHIUM_Q + 1) / 2;
             else
-                ajlen5 <= (ajlen3 >> 1);       
+                ajlen5 <= (ajlen3 >> 1);        
         end
         MULT_MODE: begin
             aj2    <= aj;
